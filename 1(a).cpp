cb07f2fa8f705be141bdc92102f7aafad787031a
mudunuripoojitha412-arch <mudunuripoojitha412@gmail.com>
2025-10-19 11:01:07 +0530

    Add files via upload

---
+++ 1(a).cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n > 0 && (n & (n - 1)) == 0)
+        printf("%d is a power of two.\n", n);
+    else
+        printf("%d is not a power of two.\n", n);
+
+    return 0;
+}
+
